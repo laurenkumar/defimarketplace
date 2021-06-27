f9147bf8d1c5e701ccdb422ee8c0c147e32a2d2c
laurenkumar <lauren_d@outlook.fr>
2021-06-27 23:24:49 +0200

    modify profile

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -64,7 +64,7 @@ function Signup() {
           auth.currentUser
             .updateProfile({
               displayName: name,
-              photoURL: `https://i.pravatar.cc/150?u=${email}`,
+              photoURL: `https://www.gravatar.com/avatar/EMAIL_MD5?d=https%3A%2F%2Fui-avatars.com%2Fapi%2F/Lasse+Rafn/128`,
             })
             .then(() => {
               setSuccess(true);
