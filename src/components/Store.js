5604c5b8d6bebb85c4d1ee88f5e1824d57425ea7
laurenkumar <lauren_d@outlook.fr>
2021-06-30 15:24:56 +0200

    move connect wallet button

--- src/components/Store.js
+++ src/components/Store.js
@@ -3,6 +3,7 @@ import "./Cart.css";
 import "./Store.css";
 import CartItem from "./CartItem";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import { useStateValue } from "../StateProvider";
 import { motion } from "framer-motion";
 import { errorAnim } from "../util";
@@ -49,106 +50,104 @@ function Store() {
       </p>
       <div className="cart__inner">
         <Tabs>
-    <TabList>
-      <Tab>Title 1</Tab>
-      <Tab>Title 2</Tab>
-    </TabList>
+          <TabList>
+            <Tab>Title 1</Tab>
+            <Tab>Title 2</Tab>
+          </TabList>
 
-    <TabPanel>
-      <h2>Any content 1</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
-        <div className="cart__items">
-          <h5>Recent Orders</h5>
-          {cart.map((item) => (
-            <CartItem item={item} />
-          ))}
-        </div>
-        <form className="form cart__checkout" ref={productForm}>
-          <h5 style={{ marginBottom: "1rem" }}>Add New Product</h5>
-          <div className="form__element">
-            <input id="name" name="name" type="text" required />
-            <label for="name">Product Name</label>
-          </div>
-          <div className="form__element">
-            <input id="category" name="category" type="text" required />
-            <label for="category">Product Category</label>
-          </div>
-          <div className="form__element">
-            <input id="rating" name="rating" type="text" required />
-            <label for="rating">Rating</label>
-          </div>
-          <div className="form__element">
-            <input id="url" name="url" type="text" required />
-            <label for="url">Image URL</label>
-          </div>
-          <div className="form__element">
-            <input id="price" name="price" type="text" required />
-            <label for="price">Price</label>
-          </div>
-          <div className="form__element">
-            <input
-              id="offer"
-              name="offer"
-              type="checkbox"
-              value={checked}
-              checked={checked}
-            />
-            <label
-              for="offer"
-              onClick={() => setChecked((checked) => !checked)}
-            >
-              In Offer
-            </label>
-          </div>
-          {checked && (
-            <motion.div
-              initial="initial"
-              animate="in"
-              exit="out"
-              variants={errorAnim}
-              className="form__element"
-            >
-              <input
-                id="originalPrice"
-                name="originalPrice"
-                type="text"
-                required={checked}
-              />
-              <label for="originalPrice">Original Price</label>
-            </motion.div>
-          )}
-          <h5 style={{ margin: "2rem 0 1rem 0" }}>Features</h5>
-          <div className="form__element">
-            <input id="feat1" name="feat1" type="text" required />
-            <label for="feat1">Feature 1</label>
-          </div>
-          <div className="form__element">
-            <input id="feat2" name="feat2" type="text" required />
-            <label for="feat2">Feature 2</label>
-          </div>
-          <div className="form__element">
-            <input id="feat3" name="feat3" type="text" required />
-            <label for="feat3">Feature 3</label>
-          </div>
-          <div className="form__element">
-            <input id="feat4" name="feat4" type="text" required />
-            <label for="feat4">Feature 4</label>
-          </div>
-          <div style={{ marginTop: "2rem" }} className="form__element buttons">
-            <button
-              disabled={loading}
-              className="button buttonPrimary submit"
-              type="submit"
-              onClick={(e) => handleSubmit(e)}
-            >
-              {loading ? "Adding..." : "Add Product"}
-            </button>
-          </div>
-        </form>
+          <TabPanel>
+            <div className="cart__items">
+                <h5>Recent Orders</h5>
+                {cart.map((item) => (
+                  <CartItem item={item} />
+                ))}
+              </div>
+          </TabPanel>
+          <TabPanel>
+            <form className="form cart__checkout" ref={productForm}>
+                <h5 style={{ marginBottom: "1rem" }}>Add New Product</h5>
+                <div className="form__element">
+                  <input id="name" name="name" type="text" required />
+                  <label for="name">Product Name</label>
+                </div>
+                <div className="form__element">
+                  <input id="category" name="category" type="text" required />
+                  <label for="category">Product Category</label>
+                </div>
+                <div className="form__element">
+                  <input id="rating" name="rating" type="text" required />
+                  <label for="rating">Rating</label>
+                </div>
+                <div className="form__element">
+                  <input id="url" name="url" type="text" required />
+                  <label for="url">Image URL</label>
+                </div>
+                <div className="form__element">
+                  <input id="price" name="price" type="text" required />
+                  <label for="price">Price</label>
+                </div>
+                <div className="form__element">
+                  <input
+                    id="offer"
+                    name="offer"
+                    type="checkbox"
+                    value={checked}
+                    checked={checked}
+                  />
+                  <label
+                    for="offer"
+                    onClick={() => setChecked((checked) => !checked)}
+                  >
+                    In Offer
+                  </label>
+                </div>
+                {checked && (
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={errorAnim}
+                    className="form__element"
+                  >
+                    <input
+                      id="originalPrice"
+                      name="originalPrice"
+                      type="text"
+                      required={checked}
+                    />
+                    <label for="originalPrice">Original Price</label>
+                  </motion.div>
+                )}
+                <h5 style={{ margin: "2rem 0 1rem 0" }}>Features</h5>
+                <div className="form__element">
+                  <input id="feat1" name="feat1" type="text" required />
+                  <label for="feat1">Feature 1</label>
+                </div>
+                <div className="form__element">
+                  <input id="feat2" name="feat2" type="text" required />
+                  <label for="feat2">Feature 2</label>
+                </div>
+                <div className="form__element">
+                  <input id="feat3" name="feat3" type="text" required />
+                  <label for="feat3">Feature 3</label>
+                </div>
+                <div className="form__element">
+                  <input id="feat4" name="feat4" type="text" required />
+                  <label for="feat4">Feature 4</label>
+                </div>
+                <div style={{ marginTop: "2rem" }} className="form__element buttons">
+                  <button
+                    disabled={loading}
+                    className="button buttonPrimary submit"
+                    type="submit"
+                    onClick={(e) => handleSubmit(e)}
+                  >
+                    {loading ? "Adding..." : "Add Product"}
+                  </button>
+                </div>
+              </form>
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   );
