30b8028a03c3ddfe5dcb726d3bf0632469939ab5
laurenkumar <lauren_d@outlook.fr>
2021-07-01 16:20:28 +0200

    store

--- api/products.js
+++ api/products.js
@@ -37,7 +37,7 @@ module.exports = async (request, response) => {
   
   else {
     try {
-      const products = await db.collection("users").doc(user.uid).collection("products").get();
+      const products = await db.collection("users").doc().collection("products").get();
       console.log(products)
       const productsObj = products.docs.map((doc) => ({
         id: doc.id,
