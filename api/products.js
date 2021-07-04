968ec088790df1fb5684a67c9d6e44ae61a01259
laurenkumar <lauren_d@outlook.fr>
2021-07-04 23:28:34 +0200

    finish product api

--- api/products.js
+++ api/products.js
@@ -37,7 +37,7 @@ module.exports = async (request, response) => {
   
   else {
     try {
-      const products = await db.collection("users").collection("products").get();
+      const products = await db.collectionGroup("products").get();
       console.log(products)
       const productsObj = products.docs.map((doc) => ({
         id: doc.id,
