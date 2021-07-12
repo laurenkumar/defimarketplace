3d05f85c08763fdb0afb8f55ecf7ee12e0d75dc7
laurenkumar <lauren_d@outlook.fr>
2021-07-12 14:22:01 +0200

    finish product api

--- src/utils/web3React.js
+++ src/utils/web3React.js
@@ -1,24 +1,24 @@
-import { InjectedConnector } from '@web3-react/injected-connector'
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
-import { BscConnector } from '@binance-chain/bsc-connector'
-import { ConnectorNames } from '@pancakeswap/uikit'
-import { ethers } from 'ethers'
-import getNodeUrl from './getRpcUrl'
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { BscConnector } from '@binance-chain/bsc-connector';
+import { ConnectorNames } from '@pancakeswap/uikit';
+import { ethers } from 'ethers';
+import getNodeUrl from './getRpcUrl';
 
-const POLLING_INTERVAL = 12000
-const rpcUrl = getNodeUrl()
-const chainId = parseInt(process.env.REACT_APP_CHAIN_ID, 10)
+const POLLING_INTERVAL = 12000;
+const rpcUrl = getNodeUrl();
+const chainId = parseInt(process.env.REACT_APP_CHAIN_ID, 10);
 
-const injected = new InjectedConnector({ supportedChainIds: [chainId] })
+const injected = new InjectedConnector({ supportedChainIds: [chainId] });
 
 const walletconnect = new WalletConnectConnector({
   rpc: { [chainId]: rpcUrl },
   bridge: 'https://pancakeswap.bridge.walletconnect.org/',
   qrcode: true,
   pollingInterval: POLLING_INTERVAL,
-})
+});
 
-const bscConnector = new BscConnector({ supportedChainIds: [chainId] })
+const bscConnector = new BscConnector({ supportedChainIds: [chainId] });
 
 export const connectorsByName: { [connectorName in ConnectorNames]: any } = {
   [ConnectorNames.Injected]: injected,
@@ -27,9 +27,9 @@ export const connectorsByName: { [connectorName in ConnectorNames]: any } = {
 }
 
 export const getLibrary = (provider): ethers.providers.Web3Provider => {
-  const library = new ethers.providers.Web3Provider(provider)
-  library.pollingInterval = POLLING_INTERVAL
-  return library
+  const library = new ethers.providers.Web3Provider(provider);
+  library.pollingInterval = POLLING_INTERVAL;
+  return library;
 }
 
 /**
