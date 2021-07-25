81c8465518ed5a3fb99047acb571ff5ad981c7c8
laurenkumar <lauren_d@outlook.fr>
2021-07-25 12:40:04 +0200

    modif place component store and payment card img error

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Login from "../components/auth/Login";
 import Signup from "../components/auth/Signup";
 import Welcome from "../components/auth/Welcome";
-import Store from "../components/Store";
+import Store from "../components/store/Store";
 import PasswordReset from "../components/auth/PasswordReset";
 import Profile from "../components/user/Profile";
 import {motion} from "framer-motion";
