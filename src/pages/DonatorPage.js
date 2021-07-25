df01ec4088486894e08f73c766787c82d8752f7c
laurenkumar <lauren_d@outlook.fr>
2021-07-25 17:49:54 +0200

    Donator thank you page debut

---
+++ src/pages/DonatorPage.js
@@ -0,0 +1,26 @@
+import {React, Suspense} from "react";
+import Donator3D from "../components/donator/Donator3D";
+import {Underlay, Overlay} from "../components/donator/Donator";
+import "../components/donator/Donator.css";
+import {motion} from "framer-motion";
+import {pageTransition, pageZoom} from "../util";
+
+function DonatorPage() {
+  return (
+    <motion.div
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageZoom}
+      transition={pageTransition}
+    >
+
+      <Underlay />
+      <Suspense fallback={null}>
+        <Donator3D />
+      </Suspense>
+      <Overlay />
+    </motion.div>
+  );
+}
+export default DonatorPage;
