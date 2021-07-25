e357e80185a2e06d323aef2601d967a164733cae
laurenkumar <lauren_d@outlook.fr>
2021-07-25 18:22:06 +0200

    donator page with 3d elements

--- src/components/donator/Donator3D.js
+++ src/components/donator/Donator3D.js
@@ -5,62 +5,68 @@ import {Physics, usePlane, useCompoundBody, useSphere} from "@react-three/cannon
 import {Environment, useGLTF} from "@react-three/drei"
 import {EffectComposer, SSAO} from "@react-three/postprocessing"
 
-const baubleMaterial = new THREE.MeshLambertMaterial({ metalness: 0.9, color: "#00a99f", emissive: "dark", transparent: true, opacity: 0.9 })
-const capMaterial = new THREE.MeshStandardMaterial({ metalness: 0.9, roughness: 0.1, color: "#00a99f", emissive: "#00a99f", envMapIntensity: 9 })
-const sphereGeometry = new THREE.SphereGeometry(1, 28, 28)
-const baubles = [...Array(50)].map(() => ({
-  args: [0.6, 0.6, 0.8, 0.8, 1][Math.floor(Math.random() * 5)],
-  mass: 0.7,
-  angularDamping: 0.5,
-  linearDamping: 0.95,
-}))
-
-function Bauble({ vec = new THREE.Vector3(), ...props }) {
-  const { nodes } = useGLTF("/cap.glb")
-  const [ref, api] = useCompoundBody(() => ({
-    ...props,
-    shapes: [
-      { type: "Box", position: [0, 0, 1.2 * props.args], args: new THREE.Vector3().setScalar(props.args * 0.4).toArray() },
-      { type: "Sphere", args: props.args },
-    ],
+function Donator3D() {
+  const baubleMaterial = new THREE.MeshLambertMaterial({ metalness: 0.9, color: "#00a99f", emissive: "dark", transparent: true, opacity: 0.9 })
+  const capMaterial = new THREE.MeshStandardMaterial({ metalness: 0.9, roughness: 0.1, color: "#00a99f", emissive: "#00a99f", envMapIntensity: 9 })
+  const sphereGeometry = new THREE.SphereGeometry(1, 28, 28)
+  const baubles = [...Array(50)].map(() => ({
+    args: [0.6, 0.6, 0.8, 0.8, 1][Math.floor(Math.random() * 5)],
+    mass: 0.7,
+    angularDamping: 0.5,
+    linearDamping: 0.95,
   }))
-  useEffect(() => api.position.subscribe((p) => api.applyForce(vec.set(...p).normalize().multiplyScalar(-props.args * 35).toArray(), [0, 0, 0])), [api]) // prettier-ignore
+
+  function Bauble({ vec = new THREE.Vector3(), ...props }) {
+    const { nodes } = useGLTF("/cap.glb")
+    const [ref, api] = useCompoundBody(() => ({
+      ...props,
+      shapes: [
+        { type: "Box", position: [0, 0, 1.2 * props.args], args: new THREE.Vector3().setScalar(props.args * 0.4).toArray() },
+        { type: "Sphere", args: props.args },
+      ],
+    }))
+    useEffect(() => api.position.subscribe((p) => api.applyForce(vec.set(...p).normalize().multiplyScalar(-props.args * 35).toArray(), [0, 0, 0])), [api]) // prettier-ignore
+    return (
+      <group ref={ref} dispose={null}>
+        <mesh scale={props.args} geometry={sphereGeometry} material={baubleMaterial} />
+        <mesh scale={2.5 * props.args} position={[0, 0, -1.8 * props.args]} geometry={nodes.Mesh_1.geometry} material={capMaterial} />
+      </group>
+    )
+  }
+
+  function Collisions() {
+    const viewport = useThree((state) => state.viewport)
+    usePlane(() => ({ position: [0, 0, 0], rotation: [0, 0, 0] }))
+    usePlane(() => ({ position: [0, 0, 8], rotation: [0, -Math.PI, 0] }))
+    usePlane(() => ({ position: [0, -4, 0], rotation: [-Math.PI / 2, 0, 0] }))
+    usePlane(() => ({ position: [0, 4, 0], rotation: [Math.PI / 2, 0, 0] }))
+    const [, api] = useSphere(() => ({ type: "Kinematic", args: 2 }))
+    return useFrame((state) => api.position.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 2.5))
+  }
+
   return (
-    <group ref={ref} dispose={null}>
-      <mesh scale={props.args} geometry={sphereGeometry} material={baubleMaterial} />
-      <mesh scale={2.5 * props.args} position={[0, 0, -1.8 * props.args]} geometry={nodes.Mesh_1.geometry} material={capMaterial} />
-    </group>
-  )
+    <div className="donator">
+      <Canvas
+        dpr={1.5}
+        gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
+        camera={{ position: [0, 0, 20], fov: 35, near: 10, far: 40 }}
+        onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}>
+        <ambientLight intensity={0.75} />
+        <spotLight position={[20, 20, 25]} penumbra={1} angle={0.2} color="green" />
+        <directionalLight position={[0, 5, -4]} intensity={4} />
+        <directionalLight position={[0, -15, -0]} intensity={4} color="blue" />
+        <Physics gravity={[0, 0, 0]} iterations={1} broadphase="SAP">
+          <Collisions />
+          {baubles.map((props, i) => <Bauble key={i} {...props} />) /* prettier-ignore */}
+        </Physics>
+        <Environment files="/adamsbridge.hdr" />
+        <EffectComposer multisampling={0}>
+          <SSAO samples={11} radius={30} intensity={30} luminanceInfluence={0.6} color="green" />
+          <SSAO samples={21} radius={5} intensity={30} luminanceInfluence={0.6} color="blue" />
+        </EffectComposer>
+      </Canvas>
+    </div>
+  );
 }
 
-function Collisions() {
-  const viewport = useThree((state) => state.viewport)
-  usePlane(() => ({ position: [0, 0, 0], rotation: [0, 0, 0] }))
-  usePlane(() => ({ position: [0, 0, 8], rotation: [0, -Math.PI, 0] }))
-  usePlane(() => ({ position: [0, -4, 0], rotation: [-Math.PI / 2, 0, 0] }))
-  usePlane(() => ({ position: [0, 4, 0], rotation: [Math.PI / 2, 0, 0] }))
-  const [, api] = useSphere(() => ({ type: "Kinematic", args: 2 }))
-  return useFrame((state) => api.position.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 2.5))
-}
-
-export const Donator3D = () => (
-  <Canvas
-    dpr={1.5}
-    gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
-    camera={{ position: [0, 0, 20], fov: 35, near: 10, far: 40 }}
-    onCreated={(state) => (state.gl.toneMappingExposure = 1.5)}>
-    <ambientLight intensity={0.75} />
-    <spotLight position={[20, 20, 25]} penumbra={1} angle={0.2} color="green" />
-    <directionalLight position={[0, 5, -4]} intensity={4} />
-    <directionalLight position={[0, -15, -0]} intensity={4} color="blue" />
-    <Physics gravity={[0, 0, 0]} iterations={1} broadphase="SAP">
-      <Collisions />
-      {baubles.map((props, i) => <Bauble key={i} {...props} />) /* prettier-ignore */}
-    </Physics>
-    <Environment files="/adamsbridge.hdr" />
-    <EffectComposer multisampling={0}>
-      <SSAO samples={11} radius={30} intensity={30} luminanceInfluence={0.6} color="green" />
-      <SSAO samples={21} radius={5} intensity={30} luminanceInfluence={0.6} color="blue" />
-    </EffectComposer>
-  </Canvas>
-)
+export default Donator3D;
