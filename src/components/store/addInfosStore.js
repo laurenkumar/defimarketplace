4e4c9f0b843ffc4d5b4a785ff1aed182bfa05158
laurenkumar <lauren_d@outlook.fr>
2021-07-26 11:42:41 +0200

    store functionnalities

--- src/components/store/addInfosStore.js
+++ src/components/store/addInfosStore.js
@@ -176,34 +176,6 @@ function AddInfosStore() {
             </div>
           </div>
         </form>
-
-              <form className="form cart__checkout" ref={storeForm}>
-                <div className="form__element">
-                  <input id="name" name="name" type="text" required />
-                  <label for="name">Store Name</label>
-                </div>
-
-                <div className="form__element">
-                  <input id="address" name="address" type="text" required />
-                  <label for="address">Address</label>
-                </div>
-                
-                <h5 style={{ margin: "2rem 0 1rem 0" }}>Description</h5>
-                <div className="form__element">
-                  <input id="description" name="description" type="text" required />
-                  <label for="description">Description: what are you selling ?</label>
-                </div>
-                <div style={{ marginTop: "2rem" }} className="form__element buttons">
-                  <button
-                    disabled={loading}
-                    className="button buttonPrimary submit"
-                    type="submit"
-                    onClick={(e) => handleSubmit(e)}
-                  >
-                    {loading ? "Saving..." : "Save"}
-                  </button>
-                </div>
-              </form>
             
   );
 }
